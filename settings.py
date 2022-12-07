705190a81539b8aa397392e41d4b79b2ddc02a4f
Nayeong02 <113005381+Nayeong02@users.noreply.github.com>
2022-12-07 16:55:08 +0900

    Add files via upload

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "myapp.apps.MyappConfig",
+    "myapp.apps.MyappConfig", 
     "product.apps.ProductConfig",
 ]
 
@@ -112,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS=[
+    BASE_DIR / 'static', 
+] #static 파일들이 프로젝트에 있는 경우 반드시 기술
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- views.py
+++ views.py
@@ -1,70 +1,10 @@
-from audioop import reverse
-from multiprocessing import context
-from re import X, template
-from django.http import HttpResponse, HttpResponseRedirect
-from http.client import HTTPResponse
-from django.urls import reverse
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect
-from django.template import loader
-from .models import Members
+from django.shortcuts import render
+from . models import Products
 
 # Create your views here.
-
-topics = [
-    {'title' : '동아대학교', 'data' : 'images/img1.jpg', 'price' : 15000},
-    {'title' : '꽃', 'data' : 'images/img2.jpg', 'price' :20000},
-    {'title' : '부산대교', 'data' : 'images/img3.jpg', 'price' : 25000}
-]
-
-def index(request) :
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
-    #return HttpResponse("<h1>안녕하세요 윤나영입니다</h1>")
-
-def shop(request) :
-    template = loader.get_template('shop.html')
-    return HttpResponse(template.render())
-
-def datalist(request) :
-    m = Members.objects.all().values()
-    context = {
-        'm' : m,
-    }
-    template = loader.get_template('list.html')
-    return HttpResponse(template.render(context, request))
+def product(request) :
+    p = Products.objects.all().values()
+    return render(request, 'p_list.html', {'p' : p})
 
 def add(request) :
-    template = loader.get_template('add.html')
-    return HttpResponse(template.render({},request))
-
-def addrecord(request) :
-    x = request.POST['first']
-    y = request.POST['last']
-    m = Members(firstname = x, lastname = y)
-    m.save()
-    return HttpResponseRedirect(reverse('list'))
-
-def delete(request, id) :
-    m = Members.objects.get(id=id)
-    m.delete()
-    return HttpResponseRedirect(reverse('list'))
-
-def update(request, id) :
-    m = Members.objects.get(id=id)
-    template = loader.get_template("update.html")
-    context = {'m' : m}
-    return HttpResponse(template.render(context, request))
-
-def create(request) :
-    return HttpResponse("<h1>안녕하세요 create입니다</h1>")
-
-def read(request, id) :
-    return HttpResponse("<h1>안녕하세요 read입니다</h1>" + id)
-
-def shop(request):
-    global topics
-    template = loader.get_template('shop.html')
-    context = {
-        't' : topics,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'insert.html')
